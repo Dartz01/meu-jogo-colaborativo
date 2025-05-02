3666ada6d3c9d0b8b0fde8ef87daf8ff40c6f2eb
Dartz01 <andreluizla017@gmail.com>
2025-05-02 19:22:01 -0300

    Update main.c

---
+++ codigo/main.c
@@ -0,0 +1,12 @@
+Pseudocódigo
+
+inicio
+ Mostrar "Bem vindo ao jogo!"
+ Jogador inicia o jogo
+ Enquanto jogador estiver vivo 
+  Mostrar o inimigo
+  se jogador atacar e o ataque acertar 
+  Diminuir a vida do inimigo 
+  Vida do inimigo = 0
+ Voce Venceu
+fim
